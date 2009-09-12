icsharpcode/AvalonEdit

commit 778ee898e27e6552fbc92243e3580a5992c3d0af

Build simple folding support (independent of parsers in SharpDevelop) into AvalonEdit. Add TextEditorOptions.AllowScrollBelowDocument (enabled in SharpDevelop) to avoid the view from jumping when collapsing code near the end of document.
git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@4910 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

ICSharpCode.AvalonEdit/TextEditor.cs
@@ -14,13 +14,13 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Markup;
+using System.Windows.Threading;
 
 using ICSharpCode.AvalonEdit.Document;
 using ICSharpCode.AvalonEdit.Editing;
 using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Rendering;
 using ICSharpCode.AvalonEdit.Utils;
-using System.Windows.Threading;
 
 namespace ICSharpCode.AvalonEdit
 {
@@ -353,11 +353,18 @@ namespace ICSharpCode.AvalonEdit
 		
 		static void OnIsReadOnlyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
 		{
-			TextEditor editor = (TextEditor)d;
-			if ((bool)e.NewValue)
-				editor.TextArea.ReadOnlySectionProvider = ReadOnlyDocument.Instance;
-			else
-				editor.TextArea.ReadOnlySectionProvider = NoReadOnlySections.Instance;
+			TextEditor editor = d as TextEditor;
+			if (editor != null) {
+				if ((bool)e.NewValue)
+					editor.TextArea.ReadOnlySectionProvider = ReadOnlyDocument.Instance;
+				else
+					editor.TextArea.ReadOnlySectionProvider = NoReadOnlySections.Instance;
+				
+				TextEditorAutomationPeer peer = TextEditorAutomationPeer.FromElement(editor) as TextEditorAutomationPeer;
+				if (peer != null) {
+					peer.RaiseIsReadOnlyChanged((bool)e.OldValue, (bool)e.NewValue);
+				}
+			}
 		}
 		#endregion
 		
@@ -923,6 +930,15 @@ namespace ICSharpCode.AvalonEdit
 		}
 		
 		/// <summary>
+		/// Scrolls to the specified line.
+		/// This method requires that the TextEditor was already assigned a size (WPF layout must have run prior).
+		/// </summary>
+		public void ScrollToLine(int line)
+		{
+			ScrollTo(line, -1);
+		}
+		
+		/// <summary>
 		/// Scrolls to the specified line/column.
 		/// This method requires that the TextEditor was already assigned a size (WPF layout must have run prior).
 		/// </summary>
@@ -930,19 +946,22 @@ namespace ICSharpCode.AvalonEdit
 		{
 			const double MinimumScrollPercentage = 0.3;
 			
+			ScrollToLine(line);
 			if (scrollViewer != null) {
-				Point p = textArea.TextView.GetVisualPosition(new TextViewPosition(line, column), VisualYPosition.LineMiddle);
+				Point p = textArea.TextView.GetVisualPosition(new TextViewPosition(line, Math.Max(1, column)), VisualYPosition.LineMiddle);
 				double verticalPos = p.Y - scrollViewer.ViewportHeight / 2;
 				if (Math.Abs(verticalPos - scrollViewer.VerticalOffset) > MinimumScrollPercentage * scrollViewer.ViewportHeight) {
 					scrollViewer.ScrollToVerticalOffset(Math.Max(0, verticalPos));
 				}
-				if (p.X > scrollViewer.ViewportWidth - Caret.MinimumDistanceToViewBorder * 2) {
-					double horizontalPos = Math.Max(0, p.X - scrollViewer.ViewportWidth / 2);
-					if (Math.Abs(horizontalPos - scrollViewer.HorizontalOffset) > MinimumScrollPercentage * scrollViewer.ViewportWidth) {
-						scrollViewer.ScrollToHorizontalOffset(horizontalPos);
+				if (column > 0) {
+					if (p.X > scrollViewer.ViewportWidth - Caret.MinimumDistanceToViewBorder * 2) {
+						double horizontalPos = Math.Max(0, p.X - scrollViewer.ViewportWidth / 2);
+						if (Math.Abs(horizontalPos - scrollViewer.HorizontalOffset) > MinimumScrollPercentage * scrollViewer.ViewportWidth) {
+							scrollViewer.ScrollToHorizontalOffset(horizontalPos);
+						}
+					} else {
+						scrollViewer.ScrollToHorizontalOffset(0);
 					}
-				} else {
-					scrollViewer.ScrollToHorizontalOffset(0);
 				}
 			}
 		}

ICSharpCode.AvalonEdit/Folding/BraceFoldingStrategy.cs
@@ -1,0 +1,67 @@
+﻿// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <owner name="Daniel Grunwald"/>
+//     <version>$Revision$</version>
+// </file>
+
+using System;
+using ICSharpCode.AvalonEdit.Document;
+using System.Collections.Generic;
+
+namespace ICSharpCode.AvalonEdit.Folding
+{
+	/// <summary>
+	/// Allows producing foldings from a document based on braces.
+	/// </summary>
+	public class BraceFoldingStrategy
+	{
+		/// <summary>
+		/// Gets/Sets the opening brace. The default value is '{'.
+		/// </summary>
+		public char OpeningBrace { get; set; }
+		
+		/// <summary>
+		/// Gets/Sets the closing brace. The default value is '}'.
+		/// </summary>
+		public char ClosingBrace { get; set; }
+		
+		/// <summary>
+		/// Creates a new BraceFoldingStrategy.
+		/// </summary>
+		public BraceFoldingStrategy()
+		{
+			this.OpeningBrace = '{';
+			this.ClosingBrace = '}';
+		}
+		
+		/// <summary>
+		/// Create <see cref="NewFolding"/>s for the specified document.
+		/// </summary>
+		public IEnumerable<NewFolding> CreateNewFoldings(ITextSource document)
+		{
+			List<NewFolding> newFoldings = new List<NewFolding>();
+			
+			Stack<int> startOffsets = new Stack<int>();
+			int lastNewLineOffset = 0;
+			char openingBrace = this.OpeningBrace;
+			char closingBrace = this.ClosingBrace;
+			for (int i = 0; i < document.TextLength; i++) {
+				char c = document.GetCharAt(i);
+				if (c == openingBrace) {
+					startOffsets.Push(i);
+				} else if (c == closingBrace && startOffsets.Count > 0) {
+					int startOffset = startOffsets.Pop();
+					// don't fold if opening and closing brace are on the same line
+					if (startOffset < lastNewLineOffset) {
+						newFoldings.Add(new NewFolding(startOffset, i + 1));
+					}
+				} else if (c == '\n' || c == '\r') {
+					lastNewLineOffset = i + 1;
+				}
+			}
+			newFoldings.Sort((a,b) => a.StartOffset.CompareTo(b.StartOffset));
+			return newFoldings;
+		}
+	}
+}

ICSharpCode.AvalonEdit/Folding/FoldingManager.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Threading;
 
@@ -28,14 +29,7 @@ namespace ICSharpCode.AvalonEdit.Folding
 		
 		readonly TextSegmentCollection<FoldingSection> foldings;
 		
-		/// <summary>
-		/// Creates a new FoldingManager instance.
-		/// </summary>
-		public FoldingManager(TextView textView)
-			: this(textView, ThrowUtil.CheckNotNull(textView, "textView").Document)
-		{
-		}
-		
+		#region Constructor
 		/// <summary>
 		/// Creates a new FoldingManager instance.
 		/// </summary>
@@ -49,48 +43,9 @@ namespace ICSharpCode.AvalonEdit.Folding
 			this.document = document;
 			this.foldings = new TextSegmentCollection<FoldingSection>(document);
 		}
-		
-		#region ExpandFoldingsWhenCaretIsMovedIntoThem
-		// keep a reference to the helper as long as the folding manager exists
-		ExpandFoldingsWhenCaretIsMovedIntoThemHelper helper;
-		
-		/// <summary>
-		/// Will listen to Caret.PositionChanged events and automatically expand folding sections
-		/// when the caret is moved into them.
-		/// </summary>
-		public void ExpandFoldingsWhenCaretIsMovedIntoThem(Caret caret)
-		{
-			if (helper == null)
-				helper = new ExpandFoldingsWhenCaretIsMovedIntoThemHelper(this);
-			CaretWeakEventManager.PositionChanged.AddListener(caret, helper);
-		}
-		
-		sealed class ExpandFoldingsWhenCaretIsMovedIntoThemHelper : IWeakEventListener
-		{
-			FoldingManager manager;
-			
-			public ExpandFoldingsWhenCaretIsMovedIntoThemHelper(FoldingManager manager)
-			{
-				this.manager = manager;
-			}
-			
-			bool IWeakEventListener.ReceiveWeakEvent(Type managerType, object sender, EventArgs e)
-			{
-				if (managerType == typeof(CaretWeakEventManager.PositionChanged)) {
-					Caret caret = (Caret)sender;
-					int caretOffset = caret.Offset;
-					foreach (FoldingSection s in manager.GetFoldingsContaining(caretOffset)) {
-						if (s.IsFolded && s.StartOffset < caretOffset && caretOffset < s.EndOffset) {
-							s.IsFolded = false;
-						}
-					}
-					return true;
-				}
-				return false;
-			}
-		}
 		#endregion
 		
+		#region Create / Remove / Clear
 		/// <summary>
 		/// Creates a folding for the specified text section.
 		/// </summary>
@@ -100,7 +55,7 @@ namespace ICSharpCode.AvalonEdit.Folding
 				throw new ArgumentException("startOffset must be less than endOffset");
 			FoldingSection fs = new FoldingSection(this, startOffset, endOffset);
 			foldings.Add(fs);
-			textView.Redraw();
+			textView.Redraw(fs, DispatcherPriority.Normal);
 			return fs;
 		}
 		
@@ -111,7 +66,6 @@ namespace ICSharpCode.AvalonEdit.Folding
 		{
 			if (fs == null)
 				throw new ArgumentNullException("fs");
-			document.VerifyAccess();
 			fs.IsFolded = false;
 			foldings.Remove(fs);
 			textView.Redraw(fs, DispatcherPriority.Normal);
@@ -128,7 +82,9 @@ namespace ICSharpCode.AvalonEdit.Folding
 			foldings.Clear();
 			textView.Redraw();
 		}
+		#endregion
 		
+		#region Get...Folding
 		/// <summary>
 		/// Gets all foldings in this manager.
 		/// The foldings are returned sorted by start offset;
@@ -182,5 +138,158 @@ namespace ICSharpCode.AvalonEdit.Folding
 		{
 			return foldings.FindSegmentsContaining(offset);
 		}
+		#endregion
+		
+		#region UpdateFoldings
+		/// <summary>
+		/// Updates the foldings in this <see cref="FoldingManager"/> using the given new foldings.
+		/// This method will try to detect which new foldings correspond to which existing foldings; and will keep the state
+		/// (<see cref="FoldingSection.IsFolded"/>) for existing foldings.
+		/// </summary>
+		/// <param name="newFoldings">The new set of foldings. These must be sorted by starting offset.</param>
+		/// <param name="firstErrorOffset">The first position of a parse error. Existing foldings starting after
+		/// this offset will be kept even if they don't appear in <paramref name="newFoldings"/>.
+		/// Use -1 for this parameter if there were no parse errors.</param>
+		public void UpdateFoldings(IEnumerable<NewFolding> newFoldings, int firstErrorOffset)
+		{
+			if (newFoldings == null)
+				throw new ArgumentNullException("newFoldings");
+			
+			if (firstErrorOffset < 0)
+				firstErrorOffset = int.MaxValue;
+			
+			var oldFoldings = this.AllFoldings.ToArray();
+			int oldFoldingIndex = 0;
+			int previousStartOffset = 0;
+			// merge new foldings into old foldings so that sections keep being collapsed
+			// both oldFoldings and newFoldings are sorted by start offset
+			foreach (NewFolding newFolding in newFoldings) {
+				// ensure newFoldings are sorted correctly
+				if (newFolding.StartOffset < previousStartOffset)
+					throw new ArgumentException("newFoldings must be sorted by start offset");
+				previousStartOffset = newFolding.StartOffset;
+				
+				// remove old foldings that were skipped
+				while (oldFoldingIndex < oldFoldings.Length && newFolding.StartOffset > oldFoldings[oldFoldingIndex].StartOffset) {
+					this.RemoveFolding(oldFoldings[oldFoldingIndex++]);
+				}
+				FoldingSection section;
+				// reuse current folding if its matching:
+				if (oldFoldingIndex < oldFoldings.Length && newFolding.StartOffset == oldFoldings[oldFoldingIndex].StartOffset) {
+					section = oldFoldings[oldFoldingIndex++];
+					section.Length = newFolding.EndOffset - newFolding.StartOffset;
+				} else {
+					// no matching current folding; create a new one:
+					section = this.CreateFolding(newFolding.StartOffset, newFolding.EndOffset);
+					// auto-close #regions only when opening the document
+					section.IsFolded = newFolding.DefaultClosed;
+				}
+				section.Title = newFolding.Name;
+			}
+			// remove all outstanding old foldings:
+			while (oldFoldingIndex < oldFoldings.Length) {
+				FoldingSection oldSection = oldFoldings[oldFoldingIndex];
+				if (oldSection.StartOffset >= firstErrorOffset)
+					break;
+				this.RemoveFolding(oldSection);
+			}
+		}
+		#endregion
+		
+		#region Install
+		/// <summary>
+		/// Adds Folding support to the specified text area.
+		/// Warning: The folding manager is only valid for the text area's current document. The folding manager
+		/// must be uninstalled before the text area is bound to a different document.
+		/// </summary>
+		/// <returns>The <see cref="FoldingManager"/> that manages the list of foldings inside the text area.</returns>
+		public static FoldingManager Install(TextArea textArea)
+		{
+			if (textArea == null)
+				throw new ArgumentNullException("textArea");
+			return new FoldingManagerInstallation(textArea);
+		}
+		
+		/// <summary>
+		/// Uninstalls the folding manager.
+		/// </summary>
+		/// <exception cref="ArgumentException">The specified manager was not created using <see cref="Install"/>.</exception>
+		public static void Uninstall(FoldingManager manager)
+		{
+			if (manager == null)
+				throw new ArgumentNullException("manager");
+			FoldingManagerInstallation installation = manager as FoldingManagerInstallation;
+			if (installation != null) {
+				installation.Uninstall();
+			} else {
+				throw new ArgumentException("FoldingManager was not created using FoldingManager.Install");
+			}
+		}
+		
+		sealed class FoldingManagerInstallation : FoldingManager
+		{
+			TextArea textArea;
+			FoldingMargin margin;
+			FoldingElementGenerator generator;
+			
+			public FoldingManagerInstallation(TextArea textArea) : base(textArea.TextView, textArea.Document)
+			{
+				this.textArea = textArea;
+				margin = new FoldingMargin() { FoldingManager = this };
+				generator = new FoldingElementGenerator() { FoldingManager = this };
+				textArea.LeftMargins.Add(margin);
+				textArea.TextView.ElementGenerators.Add(generator);
+				textArea.Caret.PositionChanged += textArea_Caret_PositionChanged;
+			}
+			
+			/*
+			void DemoMode()
+			{
+				foldingGenerator = new FoldingElementGenerator() { FoldingManager = fm };
+				foldingMargin = new FoldingMargin { FoldingManager = fm };
+				foldingMarginBorder = new Border {
+					Child = foldingMargin,
+					Background = new LinearGradientBrush(Colors.White, Colors.Transparent, 0)
+				};
+				foldingMarginBorder.SizeChanged += UpdateTextViewClip;
+				textEditor.TextArea.TextView.ElementGenerators.Add(foldingGenerator);
+				textEditor.TextArea.LeftMargins.Add(foldingMarginBorder);
+			}
+			
+			void UpdateTextViewClip(object sender, SizeChangedEventArgs e)
+			{
+				textEditor.TextArea.TextView.Clip = new RectangleGeometry(
+					new Rect(-foldingMarginBorder.ActualWidth,
+					         0,
+					         textEditor.TextArea.TextView.ActualWidth + foldingMarginBorder.ActualWidth,
+					         textEditor.TextArea.TextView.ActualHeight));
+			}
+			 */
+			
+			public void Uninstall()
+			{
+				Clear();
+				if (textArea != null) {
+					textArea.Caret.PositionChanged -= textArea_Caret_PositionChanged;
+					textArea.LeftMargins.Remove(margin);
+					textArea.TextView.ElementGenerators.Remove(generator);
+					margin = null;
+					generator = null;
+					textArea = null;
+				}
+			}
+			
+			void textArea_Caret_PositionChanged(object sender, EventArgs e)
+			{
+				// Expand Foldings when Caret is moved into them.
+				int caretOffset = textArea.Caret.Offset;
+				foreach (FoldingSection s in GetFoldingsContaining(caretOffset)) {
+					if (s.IsFolded && s.StartOffset < caretOffset && caretOffset < s.EndOffset) {
+						s.IsFolded = false;
+					}
+				}
+			}
+		}
+		#endregion
 	}
 }

ICSharpCode.AvalonEdit/Folding/NewFolding.cs
@@ -1,0 +1,67 @@
+﻿// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <author name="Daniel Grunwald"/>
+//     <version>$Revision$</version>
+// </file>
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Threading;
+
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+using ICSharpCode.AvalonEdit.Rendering;
+using ICSharpCode.AvalonEdit.Utils;
+
+namespace ICSharpCode.AvalonEdit.Folding
+{
+	/// <summary>
+	/// Helper class used for <see cref="FoldingManager.UpdateFoldings"/>.
+	/// </summary>
+	public sealed class NewFolding : ISegment
+	{
+		/// <summary>
+		/// Gets the start offset.
+		/// </summary>
+		public int StartOffset { get; private set; }
+		
+		/// <summary>
+		/// Gets the end offset.
+		/// </summary>
+		public int EndOffset { get; private set; }
+		
+		/// <summary>
+		/// Gets/Sets the name displayed for the folding.
+		/// </summary>
+		public string Name { get; set; }
+		
+		/// <summary>
+		/// Gets/Sets whether the folding is closed by default.
+		/// </summary>
+		public bool DefaultClosed { get; set; }
+		
+		/// <summary>
+		/// Creates a new NewFolding instance.
+		/// </summary>
+		public NewFolding(int start, int end)
+		{
+			if (!(start < end))
+				throw new ArgumentException("'start' must be less than 'end'");
+			this.StartOffset = start;
+			this.EndOffset = end;
+			this.Name = null;
+			this.DefaultClosed = false;
+		}
+		
+		int ISegment.Offset {
+			get { return this.StartOffset; }
+		}
+		
+		int ISegment.Length {
+			get { return this.EndOffset - this.StartOffset; }
+		}
+	}
+}

ICSharpCode.AvalonEdit/ICSharpCode.AvalonEdit.csproj
@@ -68,6 +68,9 @@
     <Reference Include="UIAutomationProvider">
       <RequiredTargetFramework>3.0</RequiredTargetFramework>
     </Reference>
+    <Reference Include="UIAutomationTypes">
+      <RequiredTargetFramework>3.0</RequiredTargetFramework>
+    </Reference>
     <Reference Include="WindowsBase">
       <RequiredTargetFramework>3.0</RequiredTargetFramework>
     </Reference>
@@ -135,6 +138,7 @@
     <Compile Include="Editing\DottedLineMargin.cs" />
     <Compile Include="Editing\DragDropException.cs" />
     <Compile Include="Editing\EditingCommandHandler.cs" />
+    <Compile Include="Folding\BraceFoldingStrategy.cs" />
     <Compile Include="Folding\FoldingElementGenerator.cs" />
     <Compile Include="Folding\FoldingManager.cs" />
     <Compile Include="Folding\FoldingMargin.cs" />
@@ -167,6 +171,7 @@
     <Compile Include="Editing\TextSegmentReadOnlySectionProvider.cs">
       <DependentUpon>IReadOnlySectionProvider.cs</DependentUpon>
     </Compile>
+    <Compile Include="Folding\NewFolding.cs" />
     <Compile Include="Highlighting\DocumentHighlighter.cs" />
     <Compile Include="Highlighting\HighlightedInlineBuilder.cs" />
     <Compile Include="Highlighting\HighlightedLine.cs" />

ICSharpCode.AvalonEdit/Rendering/TextView.cs
@@ -636,6 +636,10 @@ namespace ICSharpCode.AvalonEdit.Rendering
 			
 			maxWidth += AdditionalHorizontalScrollAmount;
 			double heightTreeHeight = this.DocumentHeight;
+			TextEditorOptions options = this.Options;
+			if (options.AllowScrollBelowDocument) {
+				heightTreeHeight = Math.Max(heightTreeHeight, Math.Min(heightTreeHeight - 50, scrollOffset.Y) + scrollViewport.Height);
+			}
 			
 			SetScrollData(availableSize,
 			              new Size(maxWidth, heightTreeHeight),

ICSharpCode.AvalonEdit/TextEditorAutomationPeer.cs
@@ -1,16 +1,18 @@
-/*
- * Created by SharpDevelop.
- * User: Daniel
- * Date: 04.06.2009
- * Time: 20:46
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <author name="Daniel Grunwald"/>
+//     <version>$Revision$</version>
+// </file>
 
 using System;
+using System.Diagnostics;
+using System.Windows.Automation;
 using System.Windows.Automation.Peers;
 using System.Windows.Automation.Provider;
 using System.Windows.Controls;
+
+using ICSharpCode.AvalonEdit.Utils;
 
 namespace ICSharpCode.AvalonEdit
 {
@@ -24,6 +26,7 @@ namespace ICSharpCode.AvalonEdit
 		/// </summary>
 		public TextEditorAutomationPeer(TextEditor owner) : base(owner)
 		{
+			Debug.WriteLine("TextEditorAutomationPeer was created");
 		}
 		
 		private TextEditor TextEditor {
@@ -40,7 +43,7 @@ namespace ICSharpCode.AvalonEdit
 		}
 		
 		bool IValueProvider.IsReadOnly {
-			get { return false; }
+			get { return this.TextEditor.IsReadOnly; }
 		}
 		
 		/// <inheritdoc/>
@@ -56,6 +59,11 @@ namespace ICSharpCode.AvalonEdit
 			}
 			
 			return base.GetPattern(patternInterface);
+		}
+		
+		internal void RaiseIsReadOnlyChanged(bool oldValue, bool newValue)
+		{
+			RaisePropertyChangedEvent(ValuePatternIdentifiers.IsReadOnlyProperty, Boxes.Box(oldValue), Boxes.Box(newValue));
 		}
 	}
 }

ICSharpCode.AvalonEdit/TextEditorOptions.cs
@@ -145,7 +145,7 @@ namespace ICSharpCode.AvalonEdit
 				}
 			}
 		}
-				
+		
 		bool requireControlModifierForHyperlinkClick = true;
 		
 		/// <summary>
@@ -244,6 +244,23 @@ namespace ICSharpCode.AvalonEdit
 				if (cutCopyWholeLine != value) {
 					cutCopyWholeLine = value;
 					OnPropertyChanged("CutCopyWholeLine");
+				}
+			}
+		}
+		
+		bool allowScrollBelowDocument;
+		
+		/// <summary>
+		/// Gets/Sets whether the user can scroll below the bottom of the document.
+		/// The default value is false; but it a good idea to set this property to true when using folding.
+		/// </summary>
+		[DefaultValue(false)]
+		public virtual bool AllowScrollBelowDocument {
+			get { return allowScrollBelowDocument; }
+			set {
+				if (allowScrollBelowDocument != value) {
+					allowScrollBelowDocument = value;
+					OnPropertyChanged("AllowScrollBelowDocument");
 				}
 			}
 		}

commit 3a0995584c8658d6e2dc9182fa9ccbc9437d99cb

Fixed infinite recursion in TextEditor.ScrollTo (bug introduced in r4910)
git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@4915 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

ICSharpCode.AvalonEdit/TextEditor.cs
@@ -946,7 +946,6 @@ namespace ICSharpCode.AvalonEdit
 		{
 			const double MinimumScrollPercentage = 0.3;
 			
-			ScrollToLine(line);
 			if (scrollViewer != null) {
 				Point p = textArea.TextView.GetVisualPosition(new TextViewPosition(line, Math.Max(1, column)), VisualYPosition.LineMiddle);
 				double verticalPos = p.Y - scrollViewer.ViewportHeight / 2;