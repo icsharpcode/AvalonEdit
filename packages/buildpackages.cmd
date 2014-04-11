157b5f96d8c73823bbb7d380c1ac2ea66c23d7ec
Siegfried Pammer <siegfriedpammer@gmail.com>
2014-04-11 22:32:50 +0200

    add error handling to buildpackages.cmd

--- packages/buildpackages.cmd
+++ packages/buildpackages.cmd
@@ -3,6 +3,7 @@ SETLOCAL
 SET msbuild=%windir%\microsoft.net\framework\v4.0.30319\msbuild
 SET documentation=..\Documentation
 SET project=..\ICSharpCode.AvalonEdit\ICSharpCode.AvalonEdit.csproj
+SET sample=..\ICSharpCode.AvalonEdit.Samle\ICSharpCode.AvalonEdit.Sample.csproj
 SET buildoptions=/t:Rebuild /p:Configuration=Release /p:DebugType=PdbOnly
 
 @echo Using this script requires nuget.exe to be in the PATH, and that Sandcastle Help File Builder is installed.
@@ -10,20 +11,25 @@ SET buildoptions=/t:Rebuild /p:Configuration=Release /p:DebugType=PdbOnly
 @ECHO ON
 :Clean debug build
 %msbuild% /m %project% /t:Clean /p:Configuration=Debug /p:OutputPath=%~dp0\AvalonEdit\lib\Net40
+if %errorlevel% neq 0 exit /B 1
 
 :BUILD .NET 4.0 version
 %msbuild% /m %project% %buildoptions% /p:OutputPath=%~dp0\AvalonEdit\lib\Net40
+if %errorlevel% neq 0 exit /B 1
 
 :BUILD .NET 3.5 version
 %msbuild% /m %project% %buildoptions% "/p:DefineConstants=TRACE" "/p:TargetFrameworkVersion=v3.5" /p:OutputPath=%~dp0\AvalonEdit\lib\Net35
+if %errorlevel% neq 0 exit /B 1
 
 @echo Building documentation with SHFB (for processing <inheritdoc/>)
 %msbuild% %documentation%\ICSharpCode.AvalonEdit.shfbproj
+if %errorlevel% neq 0 exit /B 1
+
 copy /Y %documentation%\Help\ICSharpCode.AvalonEdit.xml AvalonEdit\lib\Net35\ICSharpCode.AvalonEdit.xml
 copy /Y %documentation%\Help\ICSharpCode.AvalonEdit.xml AvalonEdit\lib\Net40\ICSharpCode.AvalonEdit.xml
 
 nuget.exe pack AvalonEdit.nuspec -Symbols -BasePath AvalonEdit -OutputDirectory AvalonEdit
-nuget.exe pack AvalonEdit.Sample.nuspec -BasePath AvalonEdit.Sample -OutputDirectory AvalonEdit.Sample
+rem nuget.exe pack AvalonEdit.Sample.nuspec -BasePath AvalonEdit.Sample -OutputDirectory AvalonEdit.Sample
 
 @ECHO OFF
 ENDLOCAL
