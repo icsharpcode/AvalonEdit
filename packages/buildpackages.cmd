683ee10e4739fe8f27d5d7eb0a1916dd2a8f5b13
Siegfried Pammer <siegfriedpammer@gmail.com>
2014-04-12 14:25:46 +0200

    update buildpackages.cmd

--- packages/buildpackages.cmd
+++ packages/buildpackages.cmd
@@ -5,26 +5,25 @@ SET documentation=..\Documentation
 SET project=..\ICSharpCode.AvalonEdit\ICSharpCode.AvalonEdit.csproj
 SET sample=..\ICSharpCode.AvalonEdit.Samle\ICSharpCode.AvalonEdit.Sample.csproj
 SET buildoptions=/t:Rebuild /p:Configuration=Release /p:DebugType=PdbOnly
-SET buildoptions35=/t:Rebuild /p:Configuration=ReleaseNet35 /p:DebugType=PdbOnly
 
 @echo Using this script requires nuget.exe to be in the PATH, and that Sandcastle Help File Builder is installed.
 
 @ECHO ON
 :Clean debug build
 %msbuild% /m %project% /t:Clean /p:Configuration=Debug /p:OutputPath=%~dp0\AvalonEdit\lib\Net40
-if %errorlevel% neq 0 exit /B 1
+@if %errorlevel% neq 0 exit /B 1
 
 :BUILD .NET 4.0 version
-%msbuild% /m %project% %buildoptions% /p:OutputPath=%~dp0\AvalonEdit\lib\Net40
-if %errorlevel% neq 0 exit /B 1
+%msbuild% /m %project% %buildoptions% /p:Platform=Net40 /p:OutputPath=%~dp0\AvalonEdit\lib\Net40
+@if %errorlevel% neq 0 exit /B 1
 
 :BUILD .NET 3.5 version
-%msbuild% /m %project% %buildoptions35% "/p:DefineConstants=TRACE" /p:OutputPath=%~dp0\AvalonEdit\lib\Net35
-if %errorlevel% neq 0 exit /B 1
+%msbuild% /m %project% %buildoptions% /p:Platform=Net35 "/p:DefineConstants=TRACE" /p:OutputPath=%~dp0\AvalonEdit\lib\Net35
+@if %errorlevel% neq 0 exit /B 1
 
 @echo Building documentation with SHFB (for processing <inheritdoc/>)
 %msbuild% %documentation%\ICSharpCode.AvalonEdit.shfbproj
-if %errorlevel% neq 0 exit /B 1
+@if %errorlevel% neq 0 exit /B 1
 
 copy /Y %documentation%\Help\ICSharpCode.AvalonEdit.xml AvalonEdit\lib\Net35\ICSharpCode.AvalonEdit.xml
 copy /Y %documentation%\Help\ICSharpCode.AvalonEdit.xml AvalonEdit\lib\Net40\ICSharpCode.AvalonEdit.xml
