701d78d993793a5aace7c01b2e3ded1ff45e3763
Alexander Shutov <Shutov_AS@directum.ru>
2015-07-27 17:36:39 +0400

    method TextView.MakeVisible has become a virtual .

--- ICSharpCode.AvalonEdit/Rendering/TextView.cs
+++ ICSharpCode.AvalonEdit/Rendering/TextView.cs
@@ -1619,7 +1619,7 @@ namespace ICSharpCode.AvalonEdit.Rendering
 		/// <summary>
 		/// Scrolls the text view so that the specified rectangle gets visible.
 		/// </summary>
-		public void MakeVisible(Rect rectangle)
+		public virtual void MakeVisible(Rect rectangle)
 		{
 			Rect visibleRectangle = new Rect(scrollOffset.X, scrollOffset.Y,
 			                                 scrollViewport.Width, scrollViewport.Height);
