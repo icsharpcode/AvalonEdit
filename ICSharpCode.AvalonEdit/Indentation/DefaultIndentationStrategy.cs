5c55017f1c7720d70ef2aea9ca6943f2fe777e51
Daniel Grunwald <daniel@danielgrunwald.de>
2015-12-13 13:19:57 +0100

    Fix #51: caret movement when inserting new line with DefaultIndentationStrategy and there are spaces to the right of the caret

--- ICSharpCode.AvalonEdit/Indentation/DefaultIndentationStrategy.cs
+++ ICSharpCode.AvalonEdit/Indentation/DefaultIndentationStrategy.cs
@@ -41,7 +41,9 @@ namespace ICSharpCode.AvalonEdit.Indentation
 				string indentation = document.GetText(indentationSegment);
 				// copy indentation to line
 				indentationSegment = TextUtilities.GetWhitespaceAfter(document, line.Offset);
-				document.Replace(indentationSegment, indentation);
+				document.Replace(indentationSegment.Offset, indentation.Length, indentation,
+				                 OffsetChangeMappingType.RemoveAndInsert);
+				// OffsetChangeMappingType.RemoveAndInsert guarantees the caret moves behind the new indentation.
 			}
 		}
 		
