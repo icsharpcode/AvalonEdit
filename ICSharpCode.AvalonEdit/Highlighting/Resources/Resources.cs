992d7b6086eaf70e287ffc0a8d4afb4d662daae0
HelderSepu <HelderSepu@gmail.com>
2017-03-07 11:04:49 -0500

    Add Python Definition

--- ICSharpCode.AvalonEdit/Highlighting/Resources/Resources.cs
+++ ICSharpCode.AvalonEdit/Highlighting/Resources/Resources.cs
@@ -50,6 +50,7 @@ namespace ICSharpCode.AvalonEdit.Highlighting
 			hlm.RegisterHighlighting("Patch", new[] { ".patch", ".diff" }, "Patch-Mode.xshd");
 			hlm.RegisterHighlighting("PowerShell", new[] { ".ps1", ".psm1", ".psd1" }, "PowerShell.xshd");
 			hlm.RegisterHighlighting("PHP", new[] { ".php" }, "PHP-Mode.xshd");
+			hlm.RegisterHighlighting("Python", new[] { ".py", ".pyw" }, "Python-Mode.xshd");
 			hlm.RegisterHighlighting("TeX", new[] { ".tex" }, "Tex-Mode.xshd");
 			hlm.RegisterHighlighting("VB", new[] { ".vb" }, "VB-Mode.xshd");
 			hlm.RegisterHighlighting("XML", (".xml;.xsl;.xslt;.xsd;.manifest;.config;.addin;" +
