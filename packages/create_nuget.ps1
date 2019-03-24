479b9858d47001601ecfb6685a983638b20bd362
jkuehner <jochen.kuehner@kardex.com>
2019-03-24 18:37:40 +0100

    nuget build

---
+++ packages/create_nuget.ps1
@@ -0,0 +1,24 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+
+Write-Host "root: $root"
+
+$version = [System.Reflection.Assembly]::LoadFile("$root\lib\net45\ICSharpCode.AvalonEdit.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\packages\AvalonEdit.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+$content = $content -replace '\$releasenotes\$',$env:APPVEYOR_REPO_COMMIT_MESSAGE
+
+$content | Out-File $root\packages\AvalonEdit.compiled.nuspec
+
+NuGet pack $root\packages\AvalonEdit.compiled.nuspec
+
+$content = (Get-Content $root\packages\AvalonEdit.Sample.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+$content = $content -replace '\$releasenotes\$',$env:APPVEYOR_REPO_COMMIT_MESSAGE
+
+$content | Out-File $root\packages\AvalonEdit.Sample.nuspec.compiled.nuspec
+
+NuGet pack $root\packages\AvalonEdit.Sample.nuspec.compiled.nuspec
