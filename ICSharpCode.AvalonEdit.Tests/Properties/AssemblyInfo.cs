686d5ab671a6371f9c7cded20132bcf917b036a0
jkuehner <jochen.kuehner@kardex.com>
2019-03-24 18:07:24 +0100

    feature wor on appveyor

--- ICSharpCode.AvalonEdit.Tests/Properties/AssemblyInfo.cs
+++ ICSharpCode.AvalonEdit.Tests/Properties/AssemblyInfo.cs
@@ -26,28 +26,9 @@ using NUnit.Framework;
 
 #endregion
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ICSharpCode.AvalonEdit.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ICSharpCode.AvalonEdit.Tests")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
 [assembly: ComVisible(false)]
-
-// The assembly version has following format :
-//
-// Major.Minor.Build.Revision
-//
-// You can specify all the values or you can use the default the Revision and 
-// Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0")]
 
 #if !NETCOREAPP
 // Run unit tests on STA thread.
