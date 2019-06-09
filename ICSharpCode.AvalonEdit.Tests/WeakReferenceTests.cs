3854dc2f62b2a20293b806efbe933d87256656bc
Daniel Grunwald <daniel@danielgrunwald.de>
2019-06-09 10:57:36 +0200

    WeakReferenceTests: use separate method for the setup that needs strong references
    Previously we had the problem of the compiler/JIT optimizing out the "redundant" `textView = null;` assignment, thus extending the lifetime of the strong reference and causing the test to fail.

--- ICSharpCode.AvalonEdit.Tests/WeakReferenceTests.cs
+++ ICSharpCode.AvalonEdit.Tests/WeakReferenceTests.cs
@@ -17,6 +17,7 @@
 // DEALINGS IN THE SOFTWARE.
 
 using System;
+using System.Runtime.CompilerServices;
 using System.Windows.Threading;
 using ICSharpCode.AvalonEdit.Document;
 using ICSharpCode.AvalonEdit.Editing;
@@ -31,72 +32,84 @@ namespace ICSharpCode.AvalonEdit
 		[Test]
 		public void TextViewCanBeCollectedTest()
 		{
-			TextView textView = new TextView();
-			WeakReference wr = new WeakReference(textView);
-			textView = null;
+			WeakReference wr = TextViewCanBeCollectedTest_CreateTextView();
 			GarbageCollect();
-			if (wr.IsAlive)
-				Assert.Warn("TextView is still alive, but Weakreaf could really not be tested");
+			Assert.IsFalse(wr.IsAlive);
 		}
-		
+
+		// Use separate no-inline method so that the JIT can't keep a strong
+		// reference to the text view alive past this method.
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		WeakReference TextViewCanBeCollectedTest_CreateTextView()
+		{
+			return new WeakReference(new TextView());
+		}
+
 		[Test]
 		public void DocumentDoesNotHoldReferenceToTextView()
 		{
 			TextDocument textDocument = new TextDocument();
 			Assert.AreEqual(0, textDocument.LineTrackers.Count);
 			
-			TextView textView = new TextView();
-			WeakReference wr = new WeakReference(textView);
-			textView.Document = textDocument;
+			WeakReference wr = DocumentDoesNotHoldReferenceToTextView_CreateTextView(textDocument);
 			Assert.AreEqual(1, textDocument.LineTrackers.Count);
-			textView = null;
 			
 			GarbageCollect();
-			if (wr.IsAlive)
-				Assert.Warn("TextView is still alive, but Weakreaf could really not be tested");
-			else
-			{
-				// document cannot immediately clear the line tracker
-				Assert.AreEqual(1, textDocument.LineTrackers.Count);
-				
-				// but it should clear it on the next change
-				textDocument.Insert(0, "a");
-				Assert.AreEqual(0, textDocument.LineTrackers.Count);
-			}
+			Assert.IsFalse(wr.IsAlive);
+			// document cannot immediately clear the line tracker
+			Assert.AreEqual(1, textDocument.LineTrackers.Count);
+			
+			// but it should clear it on the next change
+			textDocument.Insert(0, "a");
+			Assert.AreEqual(0, textDocument.LineTrackers.Count);
 		}
-		
+
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		WeakReference DocumentDoesNotHoldReferenceToTextView_CreateTextView(TextDocument textDocument)
+		{
+			TextView textView = new TextView();
+			textView.Document = textDocument;
+			return new WeakReference(textView);
+		}
+
 		[Test]
 		public void DocumentDoesNotHoldReferenceToTextArea()
 		{
 			TextDocument textDocument = new TextDocument();
-			
-			TextArea textArea = new TextArea();
-			WeakReference wr = new WeakReference(textArea);
-			textArea.Document = textDocument;
-			textArea = null;
+			WeakReference wr = DocumentDoesNotHoldReferenceToTextArea_CreateTextArea(textDocument);
 			
 			GarbageCollect();
-			if (wr.IsAlive)
-				Assert.Warn("TextView is still alive, but Weakreaf could really not be tested");
+			Assert.IsFalse(wr.IsAlive);
 			GC.KeepAlive(textDocument);
 		}
-		
+
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		WeakReference DocumentDoesNotHoldReferenceToTextArea_CreateTextArea(TextDocument textDocument)
+		{
+			TextArea textArea = new TextArea();
+			textArea.Document = textDocument;
+			return new WeakReference(textArea);
+		}
+
 		[Test]
 		public void DocumentDoesNotHoldReferenceToTextEditor()
 		{
 			TextDocument textDocument = new TextDocument();
-			
-			TextEditor textEditor = new TextEditor();
-			WeakReference wr = new WeakReference(textEditor);
-			textEditor.Document = textDocument;
-			textEditor = null;
+			WeakReference wr = DocumentDoesNotHoldReferenceToTextEditor_CreateTextEditor(textDocument);
 			
 			GarbageCollect();
-			if (wr.IsAlive)
-				Assert.Warn("TextEditor is still alive, but Weakreaf could really not be tested");
+			Assert.IsFalse(wr.IsAlive);
 			GC.KeepAlive(textDocument);
 		}
-		
+
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		WeakReference DocumentDoesNotHoldReferenceToTextEditor_CreateTextEditor(TextDocument textDocument)
+		{
+			TextEditor textEditor = new TextEditor();
+			textEditor.Document = textDocument;
+			return new WeakReference(textEditor);
+		}
+
 		[Test]
 		public void DocumentDoesNotHoldReferenceToLineMargin()
 		{
@@ -105,12 +118,12 @@ namespace ICSharpCode.AvalonEdit
 			WeakReference wr = DocumentDoesNotHoldReferenceToLineMargin_CreateMargin(textDocument);
 			
 			GarbageCollect();
-			if (wr.IsAlive)
-				Assert.Warn("TextView is still alive, but Weakreaf could really not be tested");
+			Assert.IsFalse(wr.IsAlive);
 			GC.KeepAlive(textDocument);
 		}
-		
+
 		// using a method to ensure the local variables can be garbage collected after the method returns
+		[MethodImpl(MethodImplOptions.NoInlining)]
 		WeakReference DocumentDoesNotHoldReferenceToLineMargin_CreateMargin(TextDocument textDocument)
 		{
 			TextView textView = new TextView() {
