9c5c737cef67658def5512bf2f36771723efd50a
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2023-02-25 18:57:20 +0100

    Prioritize longer keywords also when they're not alphanumeric

---
+++ ICSharpCode.AvalonEdit.Tests/Highlighting/XmlHighlightingDefinitionTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+
+using NUnit.Framework;
+
+namespace ICSharpCode.AvalonEdit.Tests.Highlighting
+{
+	[TestFixture]
+	public class XmlHighlightingDefinitionTests
+	{
+		[Test]
+		public void LongerKeywordsArePreferred()
+		{
+			var color = new XshdColor { Name = "Result" };
+			var syntaxDefinition = new XshdSyntaxDefinition {
+				Elements = {
+					color,
+					new XshdRuleSet {
+						Elements = { new XshdKeywords {
+								ColorReference = new XshdReference<XshdColor>(null, color.Name),
+								Words = { "foo", "foo.bar." }
+							}
+						}
+					}
+				}
+			};
+
+			var document = new TextDocument("This is a foo.bar. keyword");
+			var highlighter = new DocumentHighlighter(document, new XmlHighlightingDefinition(syntaxDefinition, HighlightingManager.Instance));
+			var result = highlighter.HighlightLine(1);
+
+			var highlightedText = document.GetText(result.Sections.Single());
+			Assert.That(highlightedText, Is.EqualTo("foo.bar."));
+		}
+	}
+}
